182cc2ac7a0263e0bc7165a0ccb013e89aaa530a
Christian Devin <christian.devin@tum.de>
2023-09-17 22:32:03 +0200

    more fix middleware

--- app/middleware.js
+++ app/middleware.js
@@ -2,21 +2,22 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
+  const express = require('express');
+  const cors = require('cors');
+  const app = express();
+
+  // Enable CORS for all routes
+  app.use(cors());
+
+  // Your other route handlers here
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
   await supabase.auth.getSession()
+
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+
   return res
 }
 
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-// Enable CORS for all routes
-app.use(cors());
-
-// Your other route handlers here
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
