d70b7725b2e0dcb6068ebdd251329d02f66025ce
Christian Devin <christian.devin@tum.de>
2023-09-17 22:55:07 +0200

    fix 3 or 4?

--- app/server.js
+++ app/server.js
@@ -1,16 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+// server.js
+const express = require('express');
+const cors = require('cors');
+const { createMiddlewareClient } = require('@supabase/auth-helpers-nextjs');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Enable CORS for all routes
-app.use(cors());
+// Custom CORS middleware function
+function enableCors(req, res, next) {
+  // Set up CORS headers
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin (adjust as needed)
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  // Continue to the next middleware/route
+  next();
+}
+
+// Use the custom CORS middleware for all routes
+app.use(enableCors);
 
 // Your API route handlers here
-// ...
 
+// Start the Express server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
