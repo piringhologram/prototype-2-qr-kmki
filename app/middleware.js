5e90bd93bf88ab76cf4336d7e355dc5ac9b58bda
Christian Devin <christian.devin@tum.de>
2023-09-17 22:26:33 +0200

    checking routing on add events in deployment

--- app/middleware.js
+++ app/middleware.js
@@ -7,3 +7,16 @@ export async function middleware(req) {
   await supabase.auth.getSession()
   return res
 }
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Your other route handlers here
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
